1db6184987fc3f7c895e589bd1587da276852367
paulo <ribeiro.paulor@gmail.com>
2013-02-26 07:14:36 -0300

    versao inicial do bundle

---
+++ BFOSFreteBraspressBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BFOS\FreteBraspressBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BFOSFreteBraspressBundle extends Bundle
+{
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+BFOSFreteBraspressBundle
+========================
+
+Symfony2 Bundle que auxiliar o cálculo do frete pela Braspress
